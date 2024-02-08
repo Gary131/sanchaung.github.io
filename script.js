12edd8e2ae5b62b3b2876992e6b0261d34757842
Gary4720 <104488048+Gary131@users.noreply.github.com>
2024-02-08 23:33:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function handle_click(element) {
+    element.innerText = "按屁啊";
+    element.style.color = "red";
+
+}
+
+function playAudio(){
+    const Audio = new Audio("Reeves's muntjac.mp3")
+    audio.play();
+}
